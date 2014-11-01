5cf2a290c1affa82d5b22fd795e730b6900fdfa2
Antoine Beaupré <anarcat@koumbit.org>
2014-10-31 21:42:45 -0400

    enable debugging in tests

--- t/test-bup-cron.sh
+++ t/test-bup-cron.sh
@@ -12,7 +12,7 @@ export BUP_DIR="$tmpdir/repo.bup"
 export GIT_DIR="$BUP_DIR"
 
 bup() { "$top/bup" "$@"; }
-bup-cron() { "$top/contrib/bup-cron" --pidfile "$tmpdir/bup-cron.pid" "$@"; }
+bup-cron() { "$top/contrib/bup-cron" -D --pidfile "$tmpdir/bup-cron.pid" "$@"; }
 
 WVPASS bup init
 WVPASS cd "$tmpdir"
